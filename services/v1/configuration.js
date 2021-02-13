83ede128d2b2cd4ede757d87acfd49afefd0580a
Prajay Verenkar <prajay@ajency.in>
2021-02-14 03:08:19 +0530

    condition to enable btn only for logged in user

--- services/v1/configuration.js
+++ services/v1/configuration.js
@@ -65,7 +65,7 @@ module.exports = {
  				}
 
  				//add snipet to the theme
- 				let snipet = '<button class="wishlist-btn btn btn--small" prod-id="{{ product.id }}" customer-id="{{ customer.id }}">Wishlist</button><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script type="text/javascript" src="https://a1a2b3df7c9f.ngrok.io/assets/js/wishlist-app.js"></script>'
+ 				let snipet = '{% if customer.id != null %}<button class="wishlist-btn btn btn--small" prod-id="{{ product.id }}" customer-id="{{ customer.id }}">Wishlist</button>{% endif %}<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script type="text/javascript" src="https://a1a2b3df7c9f.ngrok.io/assets/js/wishlist-app.js"></script>'
  				let snipetPayload =  {
 									    "key": "snippets/aj-wishlist-btn.liquid",
 									    "value": snipet
